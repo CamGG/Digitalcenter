00c0c03e4b4286b4b0cadbd79bf7d902eed896a3
Camgg <Camgg2296@gmail.com>
2025-11-13 13:45:42 -0500

    Commit front vercel nuevo

--- academy/__init__.py
+++ academy/__init__.py
@@ -18,7 +18,8 @@ def create_app(config_class=Config):
     # --- ESTA ES LA SECCIÓN CORREGIDA ---
     # Define las URLs (orígenes) que tienen permiso para conectarse.
     origins = ["centrodeinnovacion.digital",
-        "https://www.centrodeinnovacion.digital",  # Tu frontend en producción
+        "https://www.centrodeinnovacion.digital", 
+        "https://digitalcenterfrontedn.vercel.app/", # Tu frontend en producción
         "http://127.0.0.1:5500",                      # Para desarrollo local con VS Code Live Server
         "http://localhost:5500",                     # Alternativa para Live Server
         "http://localhost:8000"                      # Para desarrollo local con servidor de Python
